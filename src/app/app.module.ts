49b86e471f31d8c6eb2f79d052d1605459846a40
Donal <lyr.x.informatique@gmail.com>
2020-05-10 13:05:50 +0200

    implement header and list of recipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
@@ -19,6 +20,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
     RecipeDetailComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
+    RecipeItemComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
